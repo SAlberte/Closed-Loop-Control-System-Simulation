d5ebb99f4dd23b179d02a99d5c780bc3ae135ac3
Albert <albi@onet.eu>
2019-05-27 17:13:06 +0200

    Add handling more input values

--- src/symulator/MyAction.java
+++ src/symulator/MyAction.java
@@ -37,30 +37,55 @@ public class MyAction implements ActionListener
                     frame.Frequency=value;
                     value = Double.parseDouble(frame.textTime.getText());
                     frame.Time= value;
+                     value = Double.parseDouble(frame.textk.getText());
+                    frame.Kp=value;
+                    value = Double.parseDouble(frame.textT.getText());
+                    frame.Tp= value;
+                     value = Double.parseDouble(frame.texta.getText());
+                    frame.a=value;
+                    value = Double.parseDouble(frame.textB.getText());
+                    frame.B= value;
+                    value = Double.parseDouble(frame.textb.getText());
+                    frame.b= value;
+                    value = Double.parseDouble(frame.textResTime.getText());
+                    frame.ResponseTime= value;
+
                 }
                 catch (Exception s)
                 {
                     System.out.println("Must be a number");
                 }
             
-                        
+                frame.TrueResponse.clear();
+                frame.Responselist.clear();
                 frame.Pointslist.clear();
+                frame.TruePointslist.clear();
+                
                 if(frame.RadSquare.isSelected())
                 {
-                    frame.panel.getPoints(frame.Pointslist,frame.Amplitude,frame.Time);
+                    frame.panel.getPoints(frame.Pointslist,frame.Amplitude,frame.Time,frame.Responselist);
                     frame.getRectangularPlot();
+                    frame.plotMapping();
+                    frame.getResponse(1);
+                    frame.ResponseplotMapping();
                     frame.repaint();
                 }
                 else if(frame.RadTriangular.isSelected())
                 {
-                    frame.panel.getPoints(frame.Pointslist,frame.Amplitude,frame.Time);
+                    frame.panel.getPoints(frame.Pointslist,frame.Amplitude,frame.Time,frame.Responselist);
                     frame.getTriangularPlot();
+                    frame.plotMapping();
+                    frame.getResponse(2);
+                    frame.ResponseplotMapping();
                     frame.repaint();
                 }
                 else if(frame.RadHarmonic.isSelected())
                 {
-                    frame.panel.getPoints(frame.Pointslist,frame.Amplitude,frame.Time);
+                    frame.panel.getPoints(frame.Pointslist,frame.Amplitude,frame.Time,frame.Responselist);
                     frame.getHarmonicPlot();
+                    frame.plotMapping();
+                    frame.getResponse(3);
+                    frame.ResponseplotMapping();
                     frame.repaint();
                 }
                
